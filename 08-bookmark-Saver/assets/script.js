cad1faa3e64b135240366ac16564d140f17dae27
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2017-06-15 17:06:43 +0200

    using object literal pattern in project 8
    using object literal pattern in project 8

--- 08-bookmark-Saver/assets/script.js
+++ 08-bookmark-Saver/assets/script.js
@@ -1,104 +1,89 @@
 $(document).ready(function(){
-	$('body').on('load', fetchBookmarks())
-	// submit event
-	$('#myForm').on('submit', saveBookmark);
-	// save bookmark function
-	function saveBookmark(e){
-		// get form values
-		var $siteName = $('#siteName').val();
-		var $siteUrl = $('#siteURL').val();
 
-		if(!validateForm($siteName, $siteUrl)){
-			return flase;
-		}
+	var bookmarkApp = {
+		init: function(){
+			this.fetchHTML();
+			this.bindEvents();
+			this.fetchBookmarks();
+		},
+		fetchHTML: function(){
+			this.$form = $('#myForm');
+			this.$bookmarksResults = $('#bookmarksResults');
+			// this.$dangerButton = $('.btn-danger');
+		},
+		bindEvents: function(){
+			this.$form.on('submit', this.saveBookmark.bind(this));
+		},
+		saveBookmark: function(e){
+			e.preventDefault();
+			this.$siteName = this.$form.find('#siteName').val();
+			this.$siteUrl = this.$form.find('#siteURL').val();
 
-		var $bookmark = {
-			name: $siteName,
-			url: $siteUrl
-		};
-
-		if(localStorage.getItem('$bookmarks') === null){
-			// init array
-			var $bookmarks = [];
-			$bookmarks.push($bookmark);
-			//set to local storage
-			// JSON.stringify()	- turn json into string
-			localStorage.setItem('$bookmarks', JSON.stringify($bookmarks));
-		}else{
-			// get bookmarks from localStorage
-			// JSON.parse() - Turn string into Json
-			var $bookmarks = JSON.parse(localStorage.getItem('$bookmarks'));
-			// add bookmark to array
-			$bookmarks.push($bookmark);
-			// reseted back to localStorage
-			localStorage.setItem('$bookmarks', JSON.stringify($bookmarks));
-		}
-
-		$('#myForm').val('');
-		//re-fetch bookmark
-		fetchBookmarks();
-
-
-		// prevent form from submitting
-		e.preventDefault();
-	};
-
-
-	// delete function
-	function deleteBookmark(url){
-		//get bookmark from localStorage
-		var $bookmarks = JSON.parse(localStorage.getItem('$bookmarks'));
-		// loop throught bookmarks
-		for(var i=0 ; i<$bookmarks.length ; i++){
-			if($bookmarks[i].url === url){
-				$bookmarks.splice(i, 1);
+			if(!this.validateForm(this.$siteName, this.$siteUrl)){
+				return false;
 			}
-		};
-		localStorage.setItem('$bookmarks', JSON.stringify($bookmarks));
-		//re-fetch bookmark
 
-		fetchBookmarks();
-	};
+			this.$bookmark = {
+				name: this.$siteName,
+				url: this.$siteUrl
+			};
 
-
-
-	// fetch bookmarks
-	function fetchBookmarks(){
-		// prevent form from submitting
-
-		var $bookmarks = JSON.parse(localStorage.getItem('$bookmarks'));
-
-		//get output id
-		var $bookmarksResults = $('#bookmarksResults');
-		// build output
-		if($bookmarks !== null){
-			$bookmarksResults.html('');
-			for(var i=0 ; i< $bookmarks.length ; i++){
-
-				var $name = $bookmarks[i].name;
-				var $url = $bookmarks[i].url;
-
-				console.log();
-				$bookmarksResults.append(`
-					<div class="well">
-						<h3>${$name}
-							<div class="floatRight">
-								<a class="btn btn-default" target="_blank" href="${$url}">Visit</a>
-								<a class="btn btn-danger">Delete</a>
-							</div>
-						</h3>
-					</div>
-					`);
-				$('.btn-danger').on('click', function(){
-					deleteBookmark($url);
-				});
+			if(localStorage.getItem('this.$bookmarks') === null){
+				this.$bookmarks = [];
+				this.$bookmarks.push(this.$bookmark);
+				localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
+			}else{
+				// console.log();
+				this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
+				this.$bookmarks.push(this.$bookmark);
+				localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
 			}
-		}
-	};
 
-	// validate form
-	function validateForm($siteName, $siteUrl){
-		if(!$siteName || !$siteUrl){
+			this.$form.trigger("reset");
+
+			this.fetchBookmarks();
+
+		},
+		fetchBookmarks: function(){
+			this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks')).sort(function(a, b){
+				if(a.name < b.name) return -1;
+				if(a.name > b.name) return 1;
+				return 0;
+			});
+
+			if(this.$bookmarks !== null){
+				this.$bookmarksResults.html('');
+				for(var i=0 ; i<this.$bookmarks.length ; i++){
+					var $name = this.$bookmarks[i].name;
+					var $url = this.$bookmarks[i].url;
+					this.$bookmarksResults.append(`
+						<div class="well">
+							<h3>${$name}
+								<div class="floatRight">
+									<a class="btn btn-default" target="_blank" href="${$url}">Visit</a>
+									<a class="btn btn-danger">Delete</a>
+								</div>
+							</h3>
+						</div>
+						`);
+					$('.btn-danger').on('click', function(){
+						bookmarkApp.deleteBookmark(`${$url}`);
+					});
+				}
+			}
+		},
+		deleteBookmark: function(url){
+			this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
+			for(var i=0 ; i<this.$bookmarks.length ; i++){
+				if(this.$bookmarks[i].url === url){
+					bookmarkApp.$bookmarks.splice(i, 1);
+				}
+			}
+			localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
+			bookmarkApp.fetchBookmarks();
+		},
+		validateForm: function($siteName, $siteUrl){
+			if(!$siteName || !$siteUrl){
 			alert('plese fill in the form');
 			return false;
 		}
@@ -110,13 +95,10 @@ $(document).ready(function(){
 		}
 
 		return true;
-	}
+		}
+	};
+
+	bookmarkApp.init();
+
+
 });
-
-
-// local storage Test, it's only store string so we need to parse object to string
-// localStorage.setItem('test', 'Hello world');
-// console.log(localStorage.getItem('test'));
-// localStorage.removeItem('test');
-// console.log(localStorage.getItem('test'));
-// test if bookmarks is null
