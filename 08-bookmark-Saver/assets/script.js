4c3383bb13ebe59b6472efe99f9ce745d0404a88
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2017-07-19 02:10:45 +0200

    solve some errors
    solve some errors

--- 08-bookmark-Saver/assets/script.js
+++ 08-bookmark-Saver/assets/script.js
@@ -4,92 +4,107 @@
 *   License under the MIT license.
 */
 $(document).ready(function(){
-	var bookmarkApp = {
-		init: function(){
-			this.fetchHTML();
-			this.bindEvents();
-			this.fetchBookmarks();
-		},
-		fetchHTML: function(){
-			this.$form = $('#myForm');
-			this.$bookmarksResults = $('#bookmarksResults');
-		},
-		bindEvents: function(){
-			this.$form.on('submit', this.saveBookmark.bind(this));
-		},
-		saveBookmark: function(e){
-			e.preventDefault();
-			this.$siteName = this.$form.find('#siteName').val();
-			this.$siteUrl = this.$form.find('#siteURL').val();
-			if(!this.validateForm(this.$siteName, this.$siteUrl)){
-				return false;
-			}
-			this.$bookmark = {
-				name: this.$siteName,
-				url: this.$siteUrl
-			};
-			if(localStorage.getItem('this.$bookmarks') === null){
-				this.$bookmarks = [];
-				this.$bookmarks.push(this.$bookmark);
-				localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
-			}else{
-				// console.log();
-				this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
-				this.$bookmarks.push(this.$bookmark);
-				localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
-			}
-			this.$form.trigger("reset");
-			this.fetchBookmarks();
-		},
-		fetchBookmarks: function(){
-			this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks')).sort(function(a, b){
-				if(a.name < b.name) return -1;
-				if(a.name > b.name) return 1;
-				return 0;
-			});
-			if(this.$bookmarks !== null){
-				this.$bookmarksResults.html('');
-				for(var i=0 ; i<this.$bookmarks.length ; i++){
-					var $name = this.$bookmarks[i].name;
-					var $url = this.$bookmarks[i].url;
-					this.$bookmarksResults.append(`
-						<div class="well">
-							<h3>${$name}
-								<div class="floatRight">
-									<a class="btn btn-default" target="_blank" href="${$url}">Visit</a>
-									<a class="btn btn-danger">Delete</a>
-								</div>
-							</h3>
-						</div>
-						`);
-					$('.btn-danger').on('click', function(){
-						bookmarkApp.deleteBookmark(`${$url}`);
-					});
-				}
-			}
-		},
-		deleteBookmark: function(url){
-			this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
-			for(var i=0 ; i<this.$bookmarks.length ; i++){
-				if(this.$bookmarks[i].url === url){
-					bookmarkApp.$bookmarks.splice(i, 1);
-				}
-			}
-			localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
-			bookmarkApp.fetchBookmarks();
-		},
-		validateForm: function($siteName, $siteUrl){
-			if(!$siteName || !$siteUrl){
-			alert('plese fill in the form');
-			return false;
-		}
-		var regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
-		if (!$siteUrl.match(regex)) {
-		  alert("Please use valied url");
-		  return false;
-		}
-		return true;
-		}
-	};
-	bookmarkApp.init();
+    var app = {
+        $stopAnimation: 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+        $addAnimation: 'animated fadeIn',
+        $deleteAnimation: 'animated fadeOut',
+        $shakeAnimation: 'animated shake',
+        init(){
+            this.fetchHTML();
+            this.bindEvents();
+            this.fetchBookmarks();
+        },
+        fetchHTML(){
+            this.$form = $('#myForm');
+            this.$bookmarksResults = $('#bookmarksResults');
+        },
+        bindEvents(){
+            this.$form.on('submit', this.saveBookmark.bind(this));
+        },
+        saveBookmark(e){
+            e.preventDefault();
+            this.$siteName = this.$form.find('#siteName').val();
+            this.$siteUrl = this.$form.find('#siteURL').val();
+            if(!this.validateForm(this.$siteName, this.$siteUrl)){
+                return false;
+            }
+            this.$bookmark = {
+                name: this.$siteName,
+                url: this.$siteUrl
+            };
+            if(localStorage.getItem('this.$bookmarks') === null){
+                this.$bookmarks = [];
+                this.$bookmarks.push(this.$bookmark);
+                localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
+            }else{
+                // console.log();
+                this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
+                this.$bookmarks.push(this.$bookmark);
+                localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
+            }
+            this.$form.trigger("reset");
+            this.fetchBookmarks();
+        },
+        fetchBookmarks(){
+            this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
+            if(this.$bookmarks !== null){
+                this.$bookmarksResults.html('');
+                for(var i=0 ; i<this.$bookmarks.length ; i++){
+                    var $name = this.$bookmarks[i].name;
+                    var $url = this.$bookmarks[i].url;
+                    this.$bookmarksResults.append(`
+                        <div class="well">
+                            <h3>${$name}
+                                <div class="floatRight">
+                                    <a class="btn btn-default" target="_blank" href="${$url}">Visit</a>
+                                    <a class="btn btn-danger">Delete</a>
+                                </div>
+                            </h3>
+                        </div>
+                        `).find('.well').addClass(this.$addAnimation).one(this.$stopAnimation, function() {
+                            $('.well').removeClass(this.$addAnimation);
+                        });
+            
+                    $('.btn-danger').on('click', ()=> {
+                        $('.btn-danger').closest('.well').addClass(this.$deleteAnimation).one(this.$stopAnimation, ()=> {
+                           $(this).removeClass(this.$deleteAnimation);
+                        app.deleteBookmark(`${$url}`);
+                        });
+                    });
+                }
+            }
+        },
+        deleteBookmark(url){
+            this.$bookmarks = JSON.parse(localStorage.getItem('this.$bookmarks'));
+            for(var i=0 ; i<this.$bookmarks.length ; i++){
+                if(this.$bookmarks[i].url === url){
+                    app.$bookmarks.splice(i, 1);
+                }
+            }
+            localStorage.setItem('this.$bookmarks', JSON.stringify(this.$bookmarks));
+            app.fetchBookmarks();
+        },
+        validateForm($siteName, $siteUrl){
+            if(!$siteName || !$siteUrl){
+                app.$form.find('#siteName')
+                .addClass(this.$shakeAnimation)
+                .one(this.$stopAnimation,()=> {
+                        app.$form.find('#siteName').removeClass(this.$shakeAnimation);
+                    });
+                app.$form.find('#siteURL')
+                .addClass(this.$shakeAnimation)
+                .one(this.$stopAnimation,()=> {
+                        app.$form.find('#siteURL').removeClass(this.$shakeAnimation);
+                    });
+                return false;
+            }
+            var regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+            if (!$siteUrl.match(regex)) {
+              alert("Please use valied url");
+              return false;
+            }
+        return true;
+        }
+    };
+    app.init();
 });
